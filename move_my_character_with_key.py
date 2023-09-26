128e4083ec91932bdffe9f954f4f711112f232c3
Jung-Uiseok <tnekwjd3359@naver.com>
2023-09-26 20:00:54 +0900

    create handle_events to quit

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,4 +4,16 @@ open_canvas()
 background = load_image('TUK_GROUND.png')
 character = load_image('sprite_sheet.png')
 
+def handle_events():
+    global running
+
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running == False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running == False
+
+running = True
+
 close_canvas()
