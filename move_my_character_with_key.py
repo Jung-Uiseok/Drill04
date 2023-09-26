75d461b00d7e4bd12835c012466f385ca5cf6a5d
Jung-Uiseok <tnekwjd3359@naver.com>
2023-09-26 20:51:10 +0900

    add keydown and keyup

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,8 +5,7 @@ background = load_image('TUK_GROUND.png')
 character = load_image('SPRITE_SHEET.png')
 
 def handle_events():
-    global running
-    global x
+    global running, dir
 
     events = get_events()
     for event in events:
@@ -14,16 +13,22 @@ def handle_events():
             running == False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
-                x = x + 10
+                dir += 1
             elif event.key == SDLK_LEFT:
-                x = x - 10
+                dir -= 1
             elif event.key == SDLK_ESCAPE:
                 running = False
-
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir -= 1
+            elif event.key == SDLK_LEFT:
+                dir += 1
 
 running = True
 x = 800//2
 frame = 0
+dir = 0
+
 while running:
     clear_canvas()
     background.draw(600,100)
@@ -31,6 +36,7 @@ while running:
     update_canvas()
     handle_events()
     frame = (frame + 1) % 8
+    x += dir * 5
     delay(0.05)
 
 close_canvas()
