da3d238a81210561d9277fb6a126100c7fb0d846
Jung-Uiseok <tnekwjd3359@naver.com>
2023-09-26 20:21:40 +0900

    add event type keydown

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -12,23 +12,24 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running == False
-        elif event.type == SDLK_ESCAPE:
-            running == False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                x += 10
+            elif event.key == SDLK_LEFT:
+                x -= 10
+            elif event.key == SDLK_ESCAPE:
+                running = False
 
 
 running = True
+x = 800//2
 frame = 0
-for x in range(0,800,5):
-
+while running:
     clear_canvas()
     background.draw(600,100)
-    character.clip_draw(frame*50,0,100,100,x,130,200,200)
+    character.clip_draw(frame*50,1000,100,100,x,90)
     update_canvas()
-
     handle_events()
-    if not running:
-        break
-
     frame = (frame + 1) % 8
     delay(0.05)
 
