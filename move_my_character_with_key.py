4e4c48ee8bde925c210cf0ee721ecbc72c1d7f04
Jung-Uiseok <tnekwjd3359@naver.com>
2023-09-26 20:15:28 +0900

    draw background and character

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,14 +6,30 @@ character = load_image('sprite_sheet.png')
 
 def handle_events():
     global running
+    global x
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running == False
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+        elif event.type == SDLK_ESCAPE:
             running == False
 
+
 running = True
+frame = 0
+for x in range(0,800,5):
+
+    clear_canvas()
+    background.draw(600,100)
+    character.clip_draw(frame*50,0,100,100,x,130,200,200)
+    update_canvas()
+
+    handle_events()
+    if not running:
+        break
+
+    frame = (frame + 1) % 8
+    delay(0.05)
 
 close_canvas()
